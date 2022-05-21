c97d9726fa325565c59b2cd6ff456e54a0575e6e
Vi Truong <vitruong@Vis-MacBook-Pro.local>
2022-05-21 16:53:15 -0500

    fixed cart drop down

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
 import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
+import { CartContext } from "../../contexts/cart.context";
 
 const CartDropDown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
     </div>
   );
 };
